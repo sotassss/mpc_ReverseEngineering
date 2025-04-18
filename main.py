6da3311c6b88425654d4a159e96baa8d0c4615ce
池嶋 壮太 <1109685@dm-hikari.ad.hikari.co.jp>
2025-04-18 12:16:47 +0900

    Sample cobol

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ load_dotenv()
 
 def main():
     # ソースコードが格納されているフォルダのパス
-    source_folder = "code_sample_python"
+    source_folder = "code/code_sample_cbl"
 
     source_files = collect_all_files(source_folder) 
     
--- src/utils/collect_all_files.py
+++ src/utils/collect_all_files.py
@@ -4,6 +4,6 @@ import os
 import glob
 
 def collect_all_files(folder_path: str) -> list[str]:
-    # フォルダ配下のすべてのファイルを再帰的に収集
+    # 再帰的にファイルを収集
     pattern = os.path.join(folder_path, "**", "*")
     return [f for f in glob.glob(pattern, recursive=True) if os.path.isfile(f)]
