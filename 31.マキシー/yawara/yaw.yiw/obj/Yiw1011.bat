a8e26fd19f0752455b12bca8fbadfa76c47b803d
池嶋 壮太 <1109685@dm-hikari.ad.hikari.co.jp>
2025-05-02 14:57:41 +0900

    test2

--- 31.マキシー/yawara/yaw.yiw/obj/Yiw1011.bat
+++
@@ -1,8 +0,0 @@
-@if not "%~0"=="%~dp0.\%~nx0" start /min cmd /c,"%~dp0.\%~nx0" %* & goto :eof
-@echo off
-
-
-powershell -Command "get-content C:\MAKISHISYS\YAWOBJ\TEMP\REZEPT1.json | Set-Content -Encoding UTF8 C:\MAKISHISYS\YAWOBJ\TEMP\REZEPT.json"
-
-
-copy /y C:\MAKISHISYS\YAWOBJ\TEMP\REZEPT1.json C:\MAKISHISYS\YAWOBJ\TEMP\REZEPT3.json
